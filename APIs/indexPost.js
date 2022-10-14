f0d5f1cbd0ada2e72aefebfcf0a72e0d33235993
robs-am <roberta.amaro89@gmail.com>
2022-10-14 14:21:06 -0300

    iniciando fetch finalizando postjs

--- APIs/indexPost.js
+++ APIs/indexPost.js
@@ -3,3 +3,10 @@ const express = require('express')
 const app = express() //guarda o express inicializado
 
 app.listen('3000') //domínio do site, default
+
+//middleware
+app.use(express.json())
+
+app.route('/').post((req, res)=> res.send(req.body))
+//o navegador não consegue fazer .post, apenas o get, por isso irá falhar
+//ir até o insomia, fazer uma request post, adicionar um JSON e criar um content no Body
